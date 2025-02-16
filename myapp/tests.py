03c179ecd1ee5773b5e8670784e0e6f71b379d75
Vlad Ponomarov <vladyslav.ponomarov@workday.com>
2025-02-16 14:00:16 +0100

    add fictive tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+
+class FictiveTest(TestCase):
+    def test_fictive(self):
+        self.assertEqual(1, 1)
+
+    def test_fail(self):
+        self.assertEqual(1, 2)
